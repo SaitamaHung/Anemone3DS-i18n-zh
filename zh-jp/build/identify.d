bd1e64e3fa4f0a1a97dec77d90223b5472900a1f
百地希留耶 <68946023+FIve201@users.noreply.github.com>
2020-09-25 07:52:33 -0500

    Add files via upload

---
+++ zh-jp/build/identify.d
@@ -0,0 +1,6 @@
+identify.o: \
+ C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/identify.c \
+ C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc_internal.h \
+ C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc.h
+C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc_internal.h:
+C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc.h:
---
+++ zh-jp/build/quirc.d
@@ -0,0 +1,6 @@
+quirc.o: \
+ C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc.c \
+ C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc_internal.h \
+ C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc.h
+C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc_internal.h:
+C\:/Users/ins/Documents/3DS/work/Anemone3DS-i18n-zh-Experimental/zh-jp/source/quirc/quirc.h:
